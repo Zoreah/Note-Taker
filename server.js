c0cce7a9cdf81d58f08dab248b23d85fb9ccecae
Zoreah Burdengit config user.nameZoreah Burdengit config --global user.email zoreah001@gmail.com <zoreah001@gmail.com>
2020-12-05 19:24:41 -0500

    added routes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+app.listen(PORT, () => {
+  console.log("you've connected at http://localhost:" + PORT);
+});
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
