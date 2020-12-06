02c4875157843176d2f5e53c138a818300369b8e
Zoreah Burdengit config user.nameZoreah Burdengit config --global user.email zoreah001@gmail.com <zoreah001@gmail.com>
2020-12-05 19:43:39 -0500

    parsing code

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.listen(PORT, () => {
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
